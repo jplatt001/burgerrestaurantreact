0e1433862adcb728e0c1382be96f063a681f44ea
jplatt001 <jplatt001@citymail.cuny.edu>
2025-05-10 16:02:21 -0400

    Switch to HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles.css' // <-- your original CSS file
 
+import { HashRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
+
